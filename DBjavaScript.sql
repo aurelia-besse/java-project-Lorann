8a434a0cd0251ae72ed26ca64e4a177e0d39ad9c
aurelia-besse <aurelia.besse@viacesi.fr>
2016-06-15 11:12:35 +0200

    Update DBjavaScript.sql

--- DBjavaScript.sql
+++ DBjavaScript.sql
@@ -6,39 +6,41 @@
 # Table: Element
 #------------------------------------------------------------
 
-INSERT INTO element (url, ElementName)  
+INSERT INTO element (url, elementName)  
 VALUES  
-("sprite\bone.png","Bone"), 
-("sprite\horizontal_bone.png","HorizontalBone"),
-("sprite\vertical_bone.png","VerticalBone"), 
-("sprite\crystal_ball.png","BubbleKey"), 
-("sprite\fireball_1.png","Spell"), 
-("sprite\gate_closed.png","Door"), 
-("sprite\lorann_b.png","Lorann"), 
-("sprite\monster_1.png","Monster1"), 
-("sprite\monster_2.png","Monster2"), 
-("sprite\monster_3.png","Monster3"),
-("sprite\monser_4.png","Monster4"), 
-("sprite\purse.png","Coin");
-("sprite\floor.png", "Floor");
+("sprite/bone.png","Bone"), 
+("sprite/horizontal_bone.png","HorizontalBone"),
+("sprite/vertical_bone.png","VerticalBone"), 
+("sprite/crystal_ball.png","BubbleKey"), 
+("sprite/fireball_1.png","Spell"), 
+("sprite/gate_closed.png","Door"), 
+("sprite/lorann_b.png","Lorann"), 
+("sprite/monster_1.png","Monster1"), 
+("sprite/monster_2.png","Monster2"), 
+("sprite/monster_3.png","Monster3"),
+("sprite/monser_4.png","Monster4"), 
+("sprite/purse.png","Coin"),
+("sprite/floor.png", "Floor");
   
 
 #------------------------------------------------------------
 # Table: Map
 #------------------------------------------------------------
 
-INSERT INTO map (Name, Width, Height) 
+INSERT INTO map (mapName, width, height) 
 VALUES 
 ("Niveau 1",20,12),
 ("Niveau 2",20,12),
 ("Niveau 3",20,12),
 ("Niveau 4",20,12);
+("Niveau 5",20,12);
+
 
 #------------------------------------------------------------
 # Table: ElementMap
 #------------------------------------------------------------
 
-INSERT INTO ElementMap (X,Y, IDelement, IDmap) 
+INSERT INTO elementMap (x,y, IDelement, IDmap) 
 VALUES 
 (0,0,13,1),
 (1,0,13,1),
